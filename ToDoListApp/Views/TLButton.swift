1a8a17bce3a91bd88486b356db6b3336e5d42d6c
nnnniicckkkk <nickhurstdev28@gmail.com>
2024-04-17 14:37:09 -0700

    ViewModel for Login page
    began set up for using Firebase to log into the account
    
    added error message to form if email or password is invalid

---
+++ ToDoListApp/Views/TLButton.swift
@@ -0,0 +1,18 @@
+//
+//  TLButton.swift
+//  ToDoListApp
+//
+//  Created by Nicholas Hurst on 4/17/24.
+//
+
+import SwiftUI
+
+struct TLButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TLButton()
+}
