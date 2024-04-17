6a966794eae819e94b8f7b2d81850993b8debbce
nnnniicckkkk <nickhurstdev28@gmail.com>
2024-04-17 10:53:43 -0700

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Nicholas Hurst on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
